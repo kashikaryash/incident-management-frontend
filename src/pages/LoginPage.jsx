7d11a8b59c565384eeb865dd281813399d0e32d2
kashikaryash <kashikaryash@gmail.com>
2025-12-10 20:15:40 +0530

    apisetup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import axios from "axios"; 
 import { login } from "../services/LoginService";
 
 const LoginPage = () => {
