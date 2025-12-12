0c1dd5b58836248ce869d218988d1c3278dc30d1
kashikaryash <kashikaryash@gmail.com>
2025-12-12 14:02:59 +0530

    materialui-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#64b5f6",
-      main: "#2196f3",
-      dark: "#1976d2",
-      300: "#64b5f6",
-    },
-    grey: {
-      100: "#f5f5f5",
-      200: "#eeeeee",
-      300: "#e0e0e0",
-      400: "#bdbdbd",
-      500: "#9e9e9e",
-      600: "#757575",
-    },
-  },
-});
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <AppRoutes />
-    </ThemeProvider>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-// AFTER (in main.jsx):
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
-const rootElement = document.getElementById('root');
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#64b5f6",
+      main: "#2196f3",
+      dark: "#1976d2",
+      300: "#64b5f6",
+    },
+    grey: {
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+    },
+  },
+});
+
+const rootElement = document.getElementById("root");
+
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
-    </StrictMode>,
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StrictMode>
   );
 }
