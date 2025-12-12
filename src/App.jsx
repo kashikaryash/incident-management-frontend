602ad49add75a40351b427c9fd387241f46f6d60
kashikaryash <kashikaryash@gmail.com>
2025-12-12 15:12:51 +0530

    materialui-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import AppRoutes from "./routes/AppRoutes";
-
-function App() {
-  return <AppRoutes />;
-}
-
+import React from 'react' 
+import AppRoutes from './routes/AppRoutes'; 
+function App() 
+{ 
+  return ( 
+    <AppRoutes/> ); 
+} 
 export default App;
