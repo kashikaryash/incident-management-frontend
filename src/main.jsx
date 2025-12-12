900fe7a38e90190184d32387bc2f7854e0379fee
kashikaryash <kashikaryash@gmail.com>
2025-12-12 15:28:52 +0530

    materialui-design

--- src/main.jsx
+++ src/main.jsx
@@ -13,18 +13,15 @@ const theme = createTheme({
       light: "#64b5f6",
       main: "#2196f3",
       dark: "#1976d2",
-      300: "#64b5f6",
     },
     grey: {
-      100: "#f5f5f5",
-      200: "#eeeeee",
-      300: "#e0e0e0",
-      400: "#bdbdbd",
-      500: "#9e9e9e",
-      600: "#757575",
+      light: "#e0e0e0",
+      main: "#9e9e9e",
+      dark: "#757575",
     },
   },
 });
+
 
 const rootElement = document.getElementById("root");
 
